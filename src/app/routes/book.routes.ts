b1d8b31d16194e942a22d284b8f7548e3cc0af84
kim-a9 <kimberlysantosalves@gmail.com>
2025-07-12 18:34:34 -0300

    refatoração das rotas de livros

--- src/app/routes/book.routes.ts
+++ src/app/routes/book.routes.ts
@@ -1,26 +1,32 @@
+import { CreateBookController } from './../controller/CreateBookController';
+import { GetBookByIDController } from '../controller/GetBookByIDController';
+import { UpdateBookController } from '../controller/UpdateBookController';
+import { DeleteBookController } from '../controller/DeleteBookController';
+
 import { Router } from 'express';
-import { CreateBookController } from '../controller/CreateBookController';
-
-
-
 const router = Router();
 
-
-
 const createBookController = new CreateBookController();
-
-
+const getBookController = new GetBookByIDController();
+const updateBookController = new UpdateBookController();
+const deleteBookController = new DeleteBookController();
 
 // router.post('/books', createBookController);
 router.post('/books', async (req, res) => {
     await createBookController.handle(req, res);
 });
 
+router.get('/books/:id', async (req, res) => {
+    await getBookController.handle(req, res);
+});
 
+router.patch('/books/:id', async (req, res) => {
+    await updateBookController.handle(req, res);
+});
 
-
-
-
+router.delete('/books/:id', async (req, res) => {
+    await deleteBookController.handle(req, res);
+});
 
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import app from '../src/infra/server/server';
 // import chalk from 'chalk';
 
-app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
+app.listen(3000, 
+    () => console.log('Servidor rodando em http://localhost:3000'));
