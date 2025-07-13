f8b0e60558172a77d935690b8bdb77ce85c06686
kim-a9 <kimberlysantosalves@gmail.com>
2025-07-12 21:10:00 -0300

    adiciona tipos para mongoose nas dependências e ajusta a configuração do servidor

--- src/infra/server/server.ts
+++ src/infra/server/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import chalk from 'chalk';
 import { connectToMongo } from '../database/mongoConnect';
-// import {getAllBooks} from '../src/app/controller';
+
 
 const app = express();
 app.use(express.json());
 
 connectToMongo();
 
-// app.use('/books', getAllBooks);
 
 export default app;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import app from '../src/infra/server/server';
 // import chalk from 'chalk';
 
-app.listen(3000, 
+const PORT = 3000;
+
+app.listen( PORT, 
     () => console.log('Servidor rodando em http://localhost:3000'));
